ad683cb1cd4b194afb2fde97bf2fc8495590df68
DevHyperCoder <arduinoleo88@gmail.com>
2021-07-17 14:47:32 +0530

    Login page UI

---
+++ src/pages/_components/LoginProviderButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let provider: string;
+  export let text: string;
+  export let onClick: () => void;
+</script>
+
+<button
+  on:click={onClick}
+  class="border border-black text-xl p-3 w-full lg:w-3/5 my-2 rounded-full shadow-lg flex items-center flex-row gap-4"
+  ><img
+    class="lg:ml-32 w-6 h-6"
+    alt={provider}
+    src={`/${provider.toLowerCase()}.png`}
+  />{text}</button
+>
---
+++ src/pages/login.svelte
@@ -0,0 +1,22 @@
+<script>
+  import LoginProviderButton from "./_components/LoginProviderButton.svelte";
+</script>
+
+<div class="lg:grid lg:grid-cols-2 px-5 lg:px-0 justify-items-center h-screen">
+  <div class=" justify-center h-full items-center flex flex-col w-full">
+    <h1 class="font-bold text-4xl uppercase mb-10">Login</h1>
+    <LoginProviderButton text="Sign In With Google" provider="Google" />
+    <LoginProviderButton text="Sign In With Github" provider="GitHub" />
+    <LoginProviderButton text="Sign in Anonymously" provider="Anonymous" />
+    <LoginProviderButton text="Sign In With Email" provider="Email" />
+  </div>
+
+  <div
+    class="hidden justify-center h-full items-center lg:flex bg-primary w-full"
+  >
+    <div class="text-center">
+      <img src="login.png" class="w-3/5 h-3/5 mx-auto" alt="asdf" />
+      <p class="font-bold text-5xl mt-10 text-white">SlidCon</p>
+    </div>
+  </div>
+</div>
